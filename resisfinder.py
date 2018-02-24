9f3f70265ad1eea50295ab785c164e507ab46e81
Carles Llàcer <carles.llacer@gmail.com>
2018-02-24 08:42:26 +0100

    Wrote code that selects a random section of the whole picture

--- resisfinder.py
+++ resisfinder.py
@@ -21,6 +21,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 from PIL import Image
+import random
 
 
 def show_with_boxes(img, boxes):
@@ -36,14 +37,59 @@ def show_with_boxes(img, boxes):
 
         ax.add_patch(rect)
         
-imgs = np.genfromtxt('dataset1/dataset1_imgs.csv', delimiter=',')
-boxs = np.genfromtxt('dataset1/dataset1_boxs.csv', delimiter=',')
+#imgs = np.genfromtxt('dataset1/dataset1_imgs.csv', delimiter=',')
+#boxs = np.genfromtxt('dataset1/dataset1_boxs.csv', delimiter=',')
 
 # Resize the images?
 
 # Resize the box lists
 boxs = boxs.reshape(5, -1, 4)
 
-for i in range(imgs.shape[0]):
-    boxes = boxs[i,:]
-    show_with_boxes(imgs[i,:], boxes)
+#for i in range(imgs.shape[0]):
+#    boxes = boxs[i,:]
+#    show_with_boxes(imgs[i,:], boxes)
+    
+ntrain = 1 # sections on which to train
+dim = 48 # section size
+stride = 48 # section stride
+
+# dimensions of the whole pictures, constant for now
+picshape = 240, 240 # height and width
+
+maxi = np.floor((picshape[1] - dim) / stride) -1
+maxj = np.floor((picshape[0] - dim) / stride) -1
+
+for n in range(ntrain):
+    # Get a random image
+    img = random.randint(0, imgs.shape[0]-1)
+    
+    
+    # Indexes of the random section
+    i = random.randint(0, maxi)
+    j = random.randint(0, maxj)
+    # Section box
+    x1, y1 = i*dim, j*dim
+    x2, y2 = x1 + dim, y1 + dim
+    sec = imgs[img,:].reshape(picshape)[y1:y2, x1:x2]
+#    plt.imshow(sec,  cmap = 'gray')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
