4808b0676af3768a9e4b8161fb695904023addcc
Carles Llàcer <carles.llacer@gmail.com>
2018-02-24 11:31:11 +0100

    Created a section for sectioning

--- resisfinder.py
+++ resisfinder.py
@@ -27,23 +27,18 @@ import random
 def show_with_boxes(img, boxes):
     """ Show an image with a series of boxes drawn over it"""
     plt.figure()
-    img = img.reshape(240,240)
     fig,ax = plt.subplots(1)
     plt.imshow(img, cmap = 'gray' )
     for b in boxes:
         x, y = b[0], b[1]
         w, h = b[2] - b[0], b[3] - b[1]
         rect = patches.Rectangle((x,y), w, h, linewidth=1,edgecolor='r',facecolor='none')
-
         ax.add_patch(rect)
         
-def test_pic(model, img, box):
-    """ Predicts the position of resistors on an image"""
-    sec_dim = 48 # section size
-    stride = 48 # section stride
-    
-    maxi = int((picshape[1] - sec_dim) / stride) +1
-    maxj = int((picshape[0] - sec_dim) / stride) +1
+def make_sections(pic, sec_dim, stride):
+    """ Takes a picture and returns boxes of sections """
+    maxi = int((pic.shape[1] - sec_dim) / stride) +1
+    maxj = int((pic.shape[0] - sec_dim) / stride) +1
     boxes = []
     for i in range(maxi):
         for j in range(maxj):
@@ -51,11 +46,26 @@ def test_pic(model, img, box):
             x1, y1 = i*sec_dim, j*sec_dim
             x2, y2 = x1 + sec_dim, y1 + sec_dim
             box = np.array((x1, y1, x2, y2))
-            sec = img.reshape(picshape)[y1:y2, x1:x2].flatten() 
-            if model.predict_classes(np.array([sec]))[0]:
-                boxes.append(box)
+            boxes.append(box)
+    return boxes
+
     
-    show_with_boxes(img, boxes)
+def test_pic(model, pic):
+    """ Predicts the position of resistors on an picture"""
+    sec_dim = 48 # section size
+    stride = 48 # section stride
+    
+            
+    boxes = make_sections(pic, sec_dim, stride)
+    found = []
+    for b in boxes:
+        x1, y1 = b[0], b[1]
+        x2, y2 = b[2], b[3]
+        sec = pic[y1:y2, x1:x2].flatten() 
+        if model.predict_classes(np.array([sec]))[0]:
+            found.append(b)
+    
+    show_with_boxes(pic, found)
 
 imgs = np.genfromtxt('dataset1/dataset1_imgs.csv', delimiter=',')
 boxs = np.genfromtxt('dataset1/dataset1_boxs.csv', delimiter=',')
@@ -78,7 +88,7 @@ picshape = 240, 240 # height and width
 # Create the random sections (the training data)
 
 data_size = 8000 # sections to create
-secs = []
+data = []
 labels = []
 
 sec_dim = 48 # section size
@@ -89,24 +99,23 @@ maxj = np.floor((picshape[0] - sec_dim) / stride) + 1
 
 for n in range(data_size):
     # Get a random image index
-    img = random.randint(0, imgs.shape[0]-1)
-    # Indexes of the random section
-    i = random.randint(0, maxi-1)
-    j = random.randint(0, maxj-1)
-    # Section box
-    x1, y1 = i*sec_dim, j*sec_dim
-    x2, y2 = x1 + sec_dim, y1 + sec_dim
-    box = np.array((x1, y1, x2, y2))
+    img_ind = random.randint(0, imgs.shape[0]-1)
+    img = imgs[img_ind,:].reshape(picshape)
+
+    secs = make_sections(img.reshape(picshape), sec_dim, stride)
+    sec = random.choice(secs) # choosen section
     label = 0
-    for b in boxs[img,:]:
+    for b in boxs[img_ind,:]:
         # If the bsection exactly contains a resistor mark as 1
         # I may need to relax this definition of error later on
         # Perhaps use the overlapping area with actual resistors
-        if (box == b).all():
+        if (sec == b).all():
             label = 1
-    # It would be faster if I didn't reshape
-    sec = imgs[img,:].reshape(picshape)[y1:y2, x1:x2].flatten()
-    secs.append(sec)
+            
+    x1, y1 = sec[0], sec[1]
+    x2, y2 = sec[2], sec[3]
+    cropped = img.reshape(picshape)[y1:y2, x1:x2].flatten()
+    data.append(cropped)
     labels.append(to_categorical(label, 2))
     
  # Create the NN model
@@ -137,7 +146,7 @@ model.compile(loss="categorical_crossentropy",
 ntrain = int(data_size*0.8)
 epochs = 3
 bsize = 20
-model.fit(np.array(secs[0:ntrain]), 
+model.fit(np.array(data[0:ntrain]), 
           np.array(labels[0:ntrain]), 
           epochs=epochs, batch_size=bsize)
 
@@ -150,19 +159,15 @@ nval = int(data_size*0.2)
 #preds = model.predict_classes(np.array(secs[0:nval]), 
 #                              verbose=False)
 #acc = (preds.transpose()==np.array(labels[0:nval])).sum()/nval
-a, acc = model.evaluate(np.array(secs[0:nval]), np.array(labels[0:nval]),
+a, acc = model.evaluate(np.array(data[0:nval]), np.array(labels[0:nval]),
                         batch_size=128)
 print("Accuracy: {:4.2f} %".format(acc*100))
 
 
 i = random.randint(0, imgs.shape[0] - 1)
-test_pic(model, imgs[i,:], boxs[i,:])
+test_pic(model, imgs[i,:].reshape(240,240))
 
 
-
-
-
-    
     
 # TODO: put the model on a separate module, so that I can call
 #   a different one each time.
