b9b6e928a2a6869e7737ec00b0427cad39cb65cf
Carles Llàcer <carles.llacer@gmail.com>
2018-03-18 17:00:52 +0100

    Took data generation out of localizer module

--- localizer.py
+++ localizer.py
@@ -61,56 +61,57 @@ class Localizer():
         """Saves  model to a file """
         self.model.save(filepath)
         
-    def train(self, pics, boxs, epochs, batch_size):
+    def train(self, data, labels, epochs, batch_size):
         """ Trains the model with a list of pictures """
-        
-        picn = pics.shape[0] # number of pics
-        data_size = picn*25 # sections to create
-        data = []
-        labels = []
-        
-        sec_dim = self.input_shape[0] # section size
-        stride = self.stride # section stride
-        
-        ### Training data creation ###
-        
-        for n in range(data_size):
-            # Get a random image index
-            pic_ind = random.randint(0, picn-1)
-            pic = pics[pic_ind,:].reshape(self.picshape)
-        
-            secs = utils.make_sections(pic.reshape(self.picshape), 
-                                       sec_dim, stride)
-            sec = random.choice(secs) # choosen section
-            label = 0
-            for b in boxs[pic_ind,:]:
-                # If the bsection exactly contains a resistor mark as 1
-                # I may need to relax this definition of error later on
-                # Perhaps use the overlapping area with actual resistors
-                if (sec == b).all():
-                    label = 1
-                    
-            x1, y1 = sec[0], sec[1]
-            x2, y2 = sec[2], sec[3]
-            cropped = pic.reshape(self.picshape)[y1:y2, x1:x2].flatten()
-            data.append(cropped)
-            labels.append(to_categorical(label, 2))
-            
+#        
+#        picn = pics.shape[0] # number of pics
+#        data_size = picn*25 # sections to create from each picture
+#        data = []
+#        labels = []
+#        
+#        sec_dim = self.input_shape[0] # section size
+#        stride = self.stride # section stride
+#        
+#        ### Training data creation ###
+#        
+#        for n in range(data_size):
+#            # Get a random image index
+#            pic_ind = random.randint(0, picn-1)
+#            pic = pics[pic_ind,:].reshape(self.picshape)
+#        
+#            secs = utils.make_sections(pic.reshape(self.picshape), 
+#                                       sec_dim, stride)
+#            sec = random.choice(secs) # choosen section
+#            label = 0
+#            for b in boxs[pic_ind,:]:
+#                # If the bsection exactly contains a resistor mark as 1
+#                # I may need to relax this definition of error later on
+#                # Perhaps use the overlapping area with actual resistors
+#                if (sec == b).all():
+#                    label = 1
+#                    
+#            x1, y1 = sec[0], sec[1]
+#            x2, y2 = sec[2], sec[3]
+#            cropped = pic.reshape(self.picshape)[y1:y2, x1:x2].flatten()
+#            data.append(cropped)
+#            labels.append(to_categorical(label, 2))
+           
         #### Training ####
         
-        ntrain = int(data_size*0.8)
-        # TODO: change random.sample to random index generation
+        ntrain = int(len(data)*0.8)
+        # The images are already created at random, I probably don't need to
+        #   select the training datapoints randomly
         
         self.model.fit(np.array(data[0:ntrain]), 
-                            np.array(labels[0:ntrain]), 
-                            batch_size, epochs)
+                       np.array(labels[0:ntrain]), 
+                       batch_size, epochs)
         
         # since sections are already created at random I might not need to
         #   select the training samples randomly
         
         ### Validation ####
         
-        nval = int(data_size*0.2)
+        nval = int(len(data)*0.2)
         a, acc = self.model.evaluate(np.array(data[0:nval]), 
                                       np.array(labels[0:nval]),
                                       batch_size=128)
@@ -121,7 +122,7 @@ class Localizer():
         the predictions for each box and trhe probabilities. """
  
         secs = []
-        boxes = utils.make_sections(pic, self.input_shape[0], self.stride)
+        boxes = utils.make_sections(pic.shape, self.input_shape[0], self.stride)
         for b in boxes:
             x1, y1 = b[0], b[1]
             x2, y2 = b[2], b[3]
--- train_localizer.py
+++ train_localizer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 23 19:56:17 2018
 
-@author: carles
-"""
 
 import numpy as np
 import random
@@ -13,6 +9,8 @@ from PIL import Image
 
 import utils
 from localizer import Localizer
+
+from keras.utils.np_utils import to_categorical
 
 dataset = 4
 imgs_loc = 'datasets/dataset{0}/dataset{0}_imgs.csv'.format(dataset)
@@ -27,14 +25,50 @@ boxs = boxs.reshape(boxs.shape[0], -1, 4)
 # dimensions of the whole pictures, constant for now
 picshape = 240, 240 # height and width
 
- # Create the NN model
+# Create the NN model
  
-localizer = Localizer(input_shape=(240, 240), 
+localizer = Localizer(input_shape=(48, 48), 
                       hidden_layers=(42,30,10))
 
+# Make the training data
+picn = pics.shape[0] # number of pics
+
+sec_dim = localizer.input_shape[0] # section size
+stride = int(sec_dim/1) # section stride
+secs = utils.make_sections(picshape, sec_dim, stride)
+    
+data_size = picn*25 # sections to create from each picture
+data = []
+labels = []
+
+for n in range(data_size):
+    # Get a random image index
+    pic_ind = random.randint(0, picn-1)
+    pic = pics[pic_ind,:].reshape(picshape)
+
+    sec = random.choice(secs) # choosen section
+    x1, y1 = sec[0], sec[1]
+    x2, y2 = sec[2], sec[3]
+    cropped = pic.reshape(picshape)[y1:y2, x1:x2].flatten()
+    data.append(cropped)
+    
+    label = 0
+    for b in boxs[pic_ind,:]:
+        # If the bsection exactly contains a resistor mark as 1
+        # I may need to relax this definition of error later on
+        # Perhaps use the overlapping area with actual resistors
+        if (sec == b).all():
+            label = 1
+#    labels = [to_categorical(1 if (sec == b).all() else 0, 2) for b in boxs[pic_ind,:]]
+            
+
+    labels.append(to_categorical(label, 2))
+           
+# Train the model
+    
 epochs = 20
 batch_size = 20
-localizer.train(pics, boxs, epochs, batch_size)
+localizer.train(data, labels, epochs, batch_size)
 
 i = random.randint(0, pics.shape[0] - 1)
 pic = pics[i,:].reshape(240,240)
@@ -43,10 +77,10 @@ utils.test_localizer(localizer, pic, show_probs=True)
 
 # a hard test, just for fun
 
-resistors = Image.open('resistors.png', mode='r')
+resistors = Image.open('test_pictures/resistors.png', mode='r')
 resistors = resistors.convert(mode='F')
  
-utils.test_localizer(localizer, np.asarray(resistors), show_probs=True)
+utils.test_localizer(localizer, np.asarray(resistors), show_probs=False)
 
 localizer.save('datasets/dataset{0}/best_model'.format(dataset))
 
